06be2bc1e9d3fd27da70c23204d71271dc8db9be
hectordemedeiros@gmail.com <hectordemedeiros@gmail.com@ade2d6c8-7caa-0e38-04da-f3c032866bf5>
2013-03-16 22:51:43 +0000

    n

---
+++ trunk/BlocoDestrutivel.java
@@ -0,0 +1,64 @@
+package jogo;
+
+import java.awt.Image;
+import java.awt.Rectangle;
+
+import javax.swing.ImageIcon;
+
+public class BlocoDestrutivel {
+	private Image blocoImagem;
+	private int x,  largura;
+	private int y, altura;
+	private Upgrade upgrade;
+	
+	public BlocoDestrutivel(int x, int y){
+		
+			this.x = x;
+			this.y = y;
+			
+			ImageIcon referencia = new ImageIcon("Imagens\\_parede.jpg");
+			blocoImagem = referencia.getImage();
+			
+			this.largura = blocoImagem.getWidth(null);
+			this.altura = blocoImagem.getHeight(null);
+	}
+	public void gerarUpgrade(){
+		
+		int gerarUpgrade = (int) (1+Math.random()*3);
+		if(gerarUpgrade == 1){
+			upgrade = new Upgrade(getX(), getY());
+			
+		}	
+		
+	}
+	
+	
+
+	public Upgrade getUpgrade() {
+		gerarUpgrade();
+		return upgrade;
+	}
+	
+	public Image getBlocoImagem() {
+		return blocoImagem;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getLargura() {
+		return largura;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getAltura() {
+		return altura;
+	}
+
+	public Rectangle getBounds(){
+		return new Rectangle(x, y, largura, altura);
+	}
+
+	
+	
+}
